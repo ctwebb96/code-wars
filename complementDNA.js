a7bfdc374dabf3743fd5bad4244bac7e2de5147d
Corey <ctwebb96@gmail.com>
2022-04-13 22:41:06 -0400

    day 8 code-wars commit  kyu 7

---
+++ complementDNA.js
@@ -0,0 +1,27 @@
+function DNAStrand(dna) {
+    let match = []
+    let arr = dna.split('')
+    arr.forEach((x) => {
+        if (x === 'A') {
+            match.push('T')
+        } else if (x === 'T') {
+            match.push('A')
+        } else if (x === 'C') {
+            match.push('G')
+        } else if (x === 'G') {
+            match.push('C')
+        } else {
+            return match
+        }
+    });
+    return match.join('')
+}
+
+///
+
+
+let pairs = { 'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C' };
+
+function DNAStrand(dna) {
+    return dna.split('').map(function (v) { return pairs[v] }).join('');
+}
