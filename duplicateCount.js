7574e482f3b2455504995c1b2736ea27e57bbbbc
Corey <ctwebb96@gmail.com>
2022-05-10 15:47:52 -0400

    day 35 code-wars commit 6 kyu

---
+++ duplicateCount.js
@@ -0,0 +1,11 @@
+function duplicateCount(text){
+    return text.toLowerCase().split('').filter((val, i, arr) => {
+        return arr.indexOf(val) !== i && arr.lastIndexOf(val) === i;
+    }).length;
+}
+
+//refactor
+
+function duplicateCount(text){
+    return (text.toLowerCase().split('').sort().join('').match(/([^])\1+/g) || []).length;
+}
